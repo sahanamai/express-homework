27ca56a3ff51d0aa0ea372aed2821368e85326b2
sahana <sahanamai@gmail.com>
2022-02-02 20:08:55 -0600

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 var db_json = require('./db/db.json');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const util = require('util');
 const { v4: uuidv4 } = require('uuid');
